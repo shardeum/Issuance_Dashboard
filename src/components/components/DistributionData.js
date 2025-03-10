b03bb8037883e3a53638f28a98d4dc7cfd2071b1
kenny-io <ekeneeze3@gmail.com>
2025-03-10 16:28:28 +0400

    update to latest whitepaper data

--- src/components/components/DistributionData.js
+++ src/components/components/DistributionData.js
@@ -1,28 +1,24 @@
 export const DistributionData = [
- {
-   "Distribution": "Foundation ",
-   "SHM": 55880000,
-   "Percentage": "11"
- },
- {
-   "Distribution": "Ecosystem & Airdrops",
-   "SHM": 25400000,
-   "Percentage": "5"
- },
- {
-   "Distribution": "Team",
-   "SHM": 76200000,
-   "Percentage": "1"
- },
- {
-   "Distribution": "Sale",
-   "SHM": 91440000,
-   "Percentage": "18"
- },
- {
-   "Distribution": "Node Reward",
-   "SHM": 259080000,
-   "Percentage": "51"
- },
+  {
+    "Distribution": "Foundation ",
+    "SHM": 55880000,
+    "Percentage": "22.44"
+  },
+  {
+    "Distribution": "Ecosystem & Airdrops",
+    "SHM": 25480000,
+    "Percentage": "10.23"
+  },
+  {
+    "Distribution": "Team",
+    "SHM": 76200000,
+    "Percentage": "30.60"
+  },
+  {
+    "Distribution": "Sale",
+    "SHM": 91440000,
+    "Percentage": "36.72"
+  },
+ ]
 
-]
+
